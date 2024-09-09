4fa7b443fb956b82f9f6a4141bde2f6611123af4
FOM1 <bazhan.serega@yandex.ru>
2024-09-09 17:44:17 +1000

    09,09,2024

--- Условная конструкция. Операторы if, elif, else.py
+++ Условная конструкция. Операторы if, elif, else.py
@@ -1,9 +1,1 @@
-first = int(input("Введите число: "))
-second = int(input("Введите число: "))
-third = int(input("Введите число: "))
-if first == second and second == third:
-    print("3")
-elif first == second or first == third or second == third:
-    print("2")
-else:
-    print("0")
+import this
