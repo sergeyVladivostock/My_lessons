8830d6f4b5af9d95fbb77f387b9d02c36a1a7b29
FOM1 <bazhan.serega@yandex.ru>
2024-09-09 11:30:49 +1000

    09,09,2024

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = [1,2,3,4],12,13,"str1","str2"
+print("Immutable var: ",immutable_var)
+
+# immutable_var[2] = 15
+# print(immutable_var)
+# Кортеж - не изменяемый список элементов
+
+mutable_list = [4,5,6,"a","fool","c"]
+mutable_list[3] = 15
+mutable_list[5] = 2
+print("Mutable list: ",mutable_list)
