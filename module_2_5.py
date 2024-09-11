41d4328bb26ffab8e5f09fecb9f240f9886e3f1f
FOM1 <bazhan.serega@yandex.ru>
2024-09-11 14:57:02 +1000

    11,09,2024 2вариант

--- module_2_5.py
+++ module_2_5.py
@@ -4,5 +4,10 @@ def get_matrix(n,m,value):
         matrix.append([])
         for j in range(m):
             matrix[i].append(value)
-    print(matrix)
-get_matrix(3,5,42)
+    return matrix
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
