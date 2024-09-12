624f2844a0f8e032bfeb5511b34e1d8b1ebb327e
FOM1 <bazhan.serega@yandex.ru>
2024-09-12 15:21:15 +1000

    12,09,2024

---
+++ Дополнительное практическое задание по модулю 2.py
@@ -0,0 +1,17 @@
+import random
+a = random.randint(3, 20)
+b = []
+v = []
+q = []
+for i in range(1,a+1):
+    b.append(i)
+    v.append(i)
+for i in range(1,len(b)) :
+    for j in range(1,len(b)+1):
+        if a % (i +j) == 0 and a!=j and i < j :
+            if j != i:
+                q.append(i)
+                q.append(j)
+            if i > (a/2):
+                break
+print(a,' - ',*q)
