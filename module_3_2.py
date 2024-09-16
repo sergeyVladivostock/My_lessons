6e8292f4e320fecd424abd1685332648afdbdc91
FOM1 <bazhan.serega@yandex.ru>
2024-09-16 10:46:21 +1000

    16,09,2024

--- module_3_2.py
+++ module_3_2.py
@@ -1,34 +1,29 @@
+from encodings.utf_7 import encode
+
 def send_email (message,recipient,*,sender = "university.help@gmail.com"):
     a = (".com",".ru",".net")
-
+    b = False
+    c = False
     if "@" not in recipient or "@" not in sender:
         print("Невозможно отправить письмо с адреса", sender, " на адрес", recipient)
-
-    for i in range(0,3):
-        if a[i] in recipient and a[i] in sender:
-            break
+    for i in range(0,len(a)):
         if a[i] in recipient:
-            for j in range(0,3):
-                if a[j] in sender:
-                    break
-                else:
-                    continue
-        else:
-            for j in range(0, 3):
-                if a[j] in sender:
-                    break
-                else:
-                    continue
-
+            b = True
+            break
+    for i in range(0,len(a)):
+        if a[i] in sender:
+            c = True
+            break
+    if c == False or b == False:
+        print("Невозможно отправить письмо с адреса",sender,"на адрес",recipient)
+        return
     if sender == recipient:
         print("Нельзя отправить письмо самому себе!")
-
+        return
     if sender == "university.help@gmail.com":
         print("Письмо успешно отправлено с адреса",sender, "на адрес", recipient)
-
     else:
         print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
-
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 print("========================")
