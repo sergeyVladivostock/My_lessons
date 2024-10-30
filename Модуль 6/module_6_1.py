e93875677e31b3bc81720c4a5a133f7c2937aed9
FOM1 <bazhan.serega@yandex.ru>
2024-10-31 09:24:35 +1000

    16,09,2024

--- Модуль 6/module_6_1.py
+++ Модуль 6/module_6_1.py
@@ -1,27 +1,52 @@
-class Animal():
-    alive = True
-    fed = False
-
-    def __init__(self,name):
+class Animal:
+    def __init__(self, name):
+        self.alive = True
+        self.fed = False
         self.name = name
 
-class Plant():
-    adible = False
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
 
+class Plant:
     def __init__(self, name):
+        self.edible = False
         self.name = name
 
 class Mammal(Animal):
-    def eat(self, food):
-        self.food = food
-
-
+    pass
 
 class Predator(Animal):
+    pass
 
 class Flower(Plant):
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = False
 
 class Fruit(Plant):
-    edible = True
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True
 
 if __name__ == '__main__':
+
+    a1 = Predator('Волк с Уолл-Стрит')
+    a2 = Mammal('Хатико')
+    p1 = Flower('Цветик семицветик')
+    p2 = Fruit('Заводной апельсин')
+
+    print(a1.name)
+    print(p1.name)
+
+    print(a1.alive)
+    print(a2.fed)
+    a1.eat(p1)
+    a2.eat(p2)
+    print(a1.alive)
+    print(a2.fed)
+
